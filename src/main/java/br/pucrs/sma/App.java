d5db59c714f5f7b3c68883bad0253209d4d596f1
ViniFKroth <vinicius.kroth@ilegra.com>
2019-10-03 18:28:23 -0300

    create config file

--- src/main/java/br/pucrs/sma/App.java
+++ src/main/java/br/pucrs/sma/App.java
@@ -1,12 +1,20 @@
 package br.pucrs.sma;
 
+import br.pucrs.sma.model.ConfigDto;
 import br.pucrs.sma.util.NumberGenerator;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("############# SIMULADOR DE FILAS #############");
         System.out.println("Alunos: Israel Deorce - Larissa Martins - Vinicius Kroth");
+
+        ConfigDto config = new Gson().fromJson(readConfigFile("config.json"), ConfigDto.class);
         Simulator simulator = new Simulator();
         // testRandomNumberGenerator();
     }
@@ -14,5 +22,9 @@ public class App {
     public static void testRandomNumberGenerator() {
         for (int i = 0; i < NumberGenerator.maxRandoms; i++)
             System.out.println(NumberGenerator.getInstance().nextRandom());
+    }
+
+    public static String readConfigFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 }
