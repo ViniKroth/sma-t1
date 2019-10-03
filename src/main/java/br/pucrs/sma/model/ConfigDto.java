d5525ddff32199587f2b68f7eac03c40eb239053
ViniFKroth <vinicius.kroth@ilegra.com>
2019-10-03 18:16:26 -0300

    create modeling object

---
+++ src/main/java/br/pucrs/sma/model/ConfigDto.java
@@ -0,0 +1,14 @@
+package br.pucrs.sma.model;
+
+import br.pucrs.sma.queue.Queue;
+
+import java.util.List;
+
+public class ConfigDto {
+    private Integer maxRandoms;
+    private List<Queue> queues;
+    private Long a;
+    private Long c;
+    private Long M;
+    private Long x0;
+}
